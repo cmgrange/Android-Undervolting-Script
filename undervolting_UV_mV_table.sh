0021e054bb261b554e2238dd9bf35a034663f10e
cmgrange <cmgrange@users.noreply.github.com>
2018-04-10 04:05:03 +0300

    Update undervolting_UV_mV_table.sh

--- undervolting_UV_mV_table.sh
+++ undervolting_UV_mV_table.sh
@@ -9,7 +9,6 @@
 #  2) if u need, run benchmark/stability test/cpu monitor app. For example:
 #  adb am start -n skynet.cputhrottlingtest/skynet.cputhrottlingtest.MainActivity
 #  adb input tap 100 100
-#  adb shell su -c /system/bin/sh /sdcard/undervolting_UV_mV_table.sh 1728000
 #  3) set frequencies list for testing. For example:
 #  set list=300000 422400 652800 729600 883200 960000 1036800 1190400 1267200 1497600 1574400 1728000 1958400 2265600
 #  or
